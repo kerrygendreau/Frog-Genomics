9a0af9720244769280bb0df2be803e6c49aad11c
Kerry Gendreau <kerrygendreau@gmail.com>
2025-07-07 17:26:06 -0400

    Update extract_longest_proteins_all.sh

--- extract_longest_proteins_all.sh
+++ extract_longest_proteins_all.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+
+python_dir="/path/to/python/dir"
+
 # Run this in the parent directory containing all GCA_* folders
-
-
-# Either run this in the same directory as the genome files or write in the full path in front of "GC*"
 for dir in GC*; do
   if [ -d "$dir" ]; then
     echo "Processing $dir"
     cd "$dir" || exit
 
     # Run Python script to extract longest proteins per gene
-    python3 list_longest_protein.py genomic.gtf longest_proteins_per_gene.tsv
+    python3 $python_dir/list_longest_protein.py genomic.gtf longest_proteins_per_gene.tsv
 
     # Run Python script to extract longest protein sequences
-    python3 extract_proteins.py protein.faa longest_proteins_per_gene.tsv genomic.gtf
+    python3 $python_dir/extract_proteins.py protein.faa longest_proteins_per_gene.tsv genomic.gtf
 
     cd ..
   fi
